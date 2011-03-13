527d64990ca9dc2214dbd0f8564f32f029934c03
adamcooman@gmail.com <adamcooman@gmail.com@d2fbd850-ccee-fbfd-508f-682b2b5a58d6>
2011-03-13 15:32:27 +0000

    HTML builder setup om fout aan Zjef door te spelen

---
+++ trunk/Servlets/HTML Builder/DBHTMLFunction.java
@@ -0,0 +1,35 @@
+package login;
+
+import database.Databasable;
+import database.ID;
+
+public class DBHTMLFunction implements Databasable
+{
+	private String name;
+
+	@Override
+	public void setID(ID id)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public ID getId()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+	
+	
+}
