b92a28b9374cc660d061dee6b3342f3954da7768
zvdepoel@gmail.com <zvdepoel@gmail.com@d2fbd850-ccee-fbfd-508f-682b2b5a58d6>
2011-03-23 18:41:16 +0000

    Database interface v2.0, zie version history en DatabasableAsValue.java in de code.

--- Database/Code/Database/testing/databaseTesting/TestDatabase.java
+++ Database/Code/Database/testing/databaseTesting/TestDatabase.java
@@ -10,7 +10,7 @@ import junit.framework.TestSuite;
 
 /**
  * @author Zjef
- * @version 1.0
+ * @version 1.01
  */
 public class TestDatabase extends TestCase
 {
@@ -30,7 +30,7 @@ public class TestDatabase extends TestCase
 		db.connect();
 		
 		TestClass test=new TestClass("ZVdP");
-		TestClass2 test2=new TestClass2("ZVdP");
+		TestClass2 test2=new TestClass2("test2");
 		db.write(test);
 		db.write(test2);
 	}
@@ -52,7 +52,7 @@ public class TestDatabase extends TestCase
 	public void testIDCreation()
 	{
 		TestClass test=new TestClass("test");
-		TestClass test2=new TestClass("test1");
+		TestClass test2=new TestClass("test2");
 		db.write(test);
 		db.write(test2);
 		
@@ -60,13 +60,20 @@ public class TestDatabase extends TestCase
 		assertEquals(3,test2.getId().getValue());
 	}
 	
-	public void testRead()
+	public void testReadEmptyVector()
 	{
 		Search search=new Search(TestClass.class,"getName","ZVdP");
 		TestClass result=db.read(search);
 		
 		assertNotNull(result);
 		assertEquals("ZVdP",result.getName());
+	}
+	
+	public void testReadWithNullDatabasable()
+	{
+		TestClass2 test=db.read(new Search(TestClass2.class,"getName","test2"));
+		assertNotNull(test);
+		assertEquals("test2",test.getName());
 	}
 	
 	public void testReadRecursion()
