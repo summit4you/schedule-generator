6a13738f5eb2f706a2fda5b28eeedcd0fae1b2e5
zvdepoel@gmail.com <zvdepoel@gmail.com@d2fbd850-ccee-fbfd-508f-682b2b5a58d6>
2011-04-04 20:17:04 +0000

    FileIO vergeten bij vorige commit

---
+++ WorkSpace/SGWebsite/src/other/FileIO.java
@@ -0,0 +1,41 @@
+package other;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * @author Zjef
+ * @version 1.0
+ */
+public class FileIO 
+{
+	/**
+	 * Returns a string of the content of the file
+	 */
+	public static String readFile(String filePath)
+	{
+	    byte[] buffer = new byte[(int) new File(filePath).length()];
+	    BufferedInputStream f = null;
+	    try {
+	        f = new BufferedInputStream(new FileInputStream(filePath));
+	        f.read(buffer);
+	    } 
+	    catch (Exception e)
+	    {
+			e.printStackTrace();
+		} 
+	    finally 
+	    {
+	        if (f != null) try 
+	        { 
+	        	f.close(); 
+	        } 
+	        catch (IOException ignored) 
+	        {
+	        }
+	    }
+	    return new String(buffer);
+	}
+}
