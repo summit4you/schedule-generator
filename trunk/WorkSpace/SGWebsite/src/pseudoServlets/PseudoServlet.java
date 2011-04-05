eb98449ccc9d503592fb23119997e57638025310
zvdepoel@gmail.com <zvdepoel@gmail.com@d2fbd850-ccee-fbfd-508f-682b2b5a58d6>
2011-04-05 18:29:14 +0000

    Update van PseudoServlet

--- trunk/WorkSpace/SGWebsite/src/pseudoServlets/PseudoServlet.java
+++ trunk/WorkSpace/SGWebsite/src/pseudoServlets/PseudoServlet.java
@@ -1,14 +1,16 @@
 package pseudoServlets;
 
+import java.io.File;
 import java.util.Vector;
 import javax.servlet.http.HttpServletRequest;
 
+import database.Database;
 import other.FileIO;
 import sessionTracking.Session;
 
 /**
  * @author Zjef
- * @version 1.0
+ * @version 1.1
  */
 public abstract class PseudoServlet
 {	
@@ -18,10 +20,15 @@ public abstract class PseudoServlet
 	private static Vector<PseudoServlet> pseudoServlets;
 	
 	/**
-	 * Link to the base Servlet, without the pseudoServlet identifier tag and session id
+	 * URL of the base Servlet, without the pseudoServlet identifier tag and session id
 	 */
 	protected static String baseLink;
-	
+	/**
+	 * file location of the servlet on the server
+	 */
+	protected static String servletPath;
+	final protected static String templateFolder="SGtemplates/";
+	final protected static String otherFileFolder="SGotherFiles/";
 	/**
 	 * Vector with all the language tags to be replaced
 	 */
@@ -51,7 +58,7 @@ public abstract class PseudoServlet
 	{
 		Vector<PseudoServlet> pseudos=new Vector<PseudoServlet>(); 
 		//add your pseudoServlets to this vector!
-		//pseudos.add(new ...);
+		pseudos.add(new SingleTable(null,"Accounts",false));
 		
 		for (PseudoServlet i:pseudos)
 		{
@@ -66,20 +73,46 @@ public abstract class PseudoServlet
 	 */
 	protected void init()
 	{
-		template=(templateFile==null?"":FileIO.readFile(templateFile));
+		loadTemplate();
 	}
 	
+	private void loadTemplate()
+	{
+		template=(templateFile==null?"":FileIO.readFile(new File(new File(servletPath).getParentFile(),templateFolder+templateFile).getAbsolutePath()));
+	}
 	//*******protected static methods************************************************************************	
+	/**
+	 * @return a new database connection<br>
+	 * The database is not connected yet
+	 */
+	protected static Database getDB()
+	{
+		//TODO replace database values with a read from an xml file
+		Database db=new Database("wilma.vub.ac.be/se5_1011","se5_1011","nieveGroep");
+		return db;
+	}
+	
 	/**
 	 * Returns a string with the tag replaced by another string<br>
 	 * {} brackets are added by this method to the toBeReplaced String.
 	 */
-	protected static String replaceTags(String string,String toBeReplaced,String replacement)
+	protected static String replaceTags(String template,String toBeReplaced,String replacement)
 	{
-		return string.replaceAll("\\{"+toBeReplaced+"\\}",replacement);
+		return template.replaceAll("\\{"+toBeReplaced+"\\}",replacement);
 	}
 	
 	//*********public methods*********************************************************************
+	/**
+	 * Generates the link for this pseudoServlet<br>
+	 * This includes the base servlet's link. ( "http://serverlocation/baseServlet?sp=xxx&id=xxx" )<br>
+	 * To add extra parameters to the link, simply append them to the return value of this method (starting with a '&')
+	 */
+	public String createLink(Session session)
+	{
+		//TODO replace "id" with a final static String
+		return baseLink+"?"+pseudoServletParamTag+"="+getIdentifier()+"&"+"id="+session.getSessionID();
+	}
+	
 	/**
 	 * @return the identifier of this PseudoServlet to be placed in a link for processing by the security servlet. 
 	 */
@@ -99,30 +132,25 @@ public abstract class PseudoServlet
 
 	//*******public static methods*******************************************************************
 	/**
-	 * Call this once before using anything else, to initialize the link to the general servlet
+	 * Inits all PseudoServlets and static variables.<br>
+	 * Call this method once before using the PseudoServlets in this class.
+	 * @param baseServletLink - url to the securityServlet
+	 * @param servletContextPath - the contextpath of the securityServlet (to retrieve the absolute filepaths of the files on the server)
 	 */
-	public static void setBaseServletLink(String link)
+	public static void initEverything(String baseServletLink,String servletContextPath)
 	{
-		baseLink=link;
+		baseLink=baseServletLink.endsWith("/")?baseServletLink:baseServletLink+"/";
+		servletPath=servletContextPath;
+		
+		pseudoServlets=loadPseudoServlets();
 	}
 	
 	/**
-	 * Generates the base link for this pseudoServlet<br>
-	 * To add extra parameters to the link, simply append them to the return value of this method (starting with a '&')
-	 */
-	public static String createLink(PseudoServlet pseudoServlet,Session session)
-	{
-		//TODO replace "id" with a final static String
-		return baseLink+"?"+pseudoServletParamTag+"="+pseudoServlet.getIdentifier()+"&"+"id="+session.getSessionID();
-	}
-	
-	/**
-	 * @param link to the main servlet
 	 * @returns a vector containing all PseudoServlets declared in the package "pseudoServlets"
 	 */
 	public static Vector<PseudoServlet> getPseudoServlets()
-	{
-		return (Vector<PseudoServlet>) (pseudoServlets==null?pseudoServlets=loadPseudoServlets():pseudoServlets).clone();
+	{		
+		return (Vector<PseudoServlet>) pseudoServlets.clone();
 	}
 	
 	/**
@@ -131,7 +159,7 @@ public abstract class PseudoServlet
 	 */
 	public static PseudoServlet getPseudoServlet(String identifier)
 	{
-		for (PseudoServlet i:getPseudoServlets())
+		for (PseudoServlet i:pseudoServlets)
 		{
 			if (i.getIdentifier().equals(identifier))
 			{
@@ -147,7 +175,7 @@ public abstract class PseudoServlet
 	 */
 	public static  PseudoServlet getPseudoServlet(Class<? extends PseudoServlet> pseudoServlet)
 	{
-		for (PseudoServlet i:getPseudoServlets())
+		for (PseudoServlet i:pseudoServlets)
 		{
 			if (i.getClass().equals(pseudoServlet))
 			{
@@ -161,7 +189,7 @@ public abstract class PseudoServlet
 	/**
 	 *@return The String of the html page returned by this PseudoServlet
 	 */
-	abstract public String processRequest(RequestType type,HttpServletRequest request/*,Account account*/);
+	abstract public String processRequest(RequestType type,HttpServletRequest request,Session session);
 	
 	/**
 	 * @return name to display in the tab
