268931384f6fb89a7dd1cac42c6dfbc4dfedf6d8
adamcooman@gmail.com <adamcooman@gmail.com@d2fbd850-ccee-fbfd-508f-682b2b5a58d6>
2011-04-07 19:47:49 +0000

    language

--- trunk/WorkSpace/SGWebsite/src/language/CollectLanguageTags.java
+++ trunk/WorkSpace/SGWebsite/src/language/CollectLanguageTags.java
@@ -6,7 +6,8 @@ import other.*;
 
 public class CollectLanguageTags 
 {
-	
+	private static Vector<String> datastructure;
+	private static String datastructurepath;
 	private static Vector<String> pseudoservlets;
 	private static String pseudoservletpath;
 	private static Vector<String> templatefiles;
@@ -21,24 +22,37 @@ public class CollectLanguageTags
 	 */
 	public static void main(String[] args)
 	{
+		datastructurepath = "C:/SVN/WorkSpace/SGWebsite/src/dataStructure/";
 		pseudoservletpath = "C:/SVN/WorkSpace/SGWebsite/src/pseudoServlets/";
 		templatefilepath = "C:/SGtemplates/";
 		languagefilepath = "C:/SVN/WorkSpace/SGWebsite/src/language/";
 		makefilenames();
 		languagereferences = new Vector<String>();
 		
+		//
+		for (String i : datastructure)
+		{
+			System.out.println("File: "+i);
+			addlanguagetags(datastructurepath+i);
+		}
 		
 		// voeg de languagefiles uit de pseudoservlets toe
 		for (String i : pseudoservlets)
 		{
+			System.out.println("File: "+i);
 			addlanguagetags(pseudoservletpath+i);
 		}
-		
 		// voeg de language files uit de templates toe
 		for (String i : templatefiles)
 		{
+			System.out.println("File: "+i);
 			addlanguagetags(templatefilepath+i);
 		}
+		
+		// haal de language references uit site.xml
+		System.out.println("File: Site.xml");
+		addlanguagetags("C:/SVN/WorkSpace/SGWebsite/src/htmlBuilder/site.xml");
+		
 		
 		// steek de language tags in een string die in een .l bestand kan gezet worden.
 		String out = new String();
@@ -53,18 +67,13 @@ public class CollectLanguageTags
 	{
 		String content = FileIO.readFile(filename);
 		// zoek naar de eerste language tag in de code
-		int hekje1 = 1;
-		int hekje2 = 1;
-		while ((content.indexOf("##", hekje2+1)!=-1)&&(content.indexOf("##", hekje1+1)!=-1))
+		String[] arr = content.split("##");
+		for (int i = 1; i < arr.length; i+=2)
 		{
-			hekje1=content.lastIndexOf("##", hekje2+1);
-			hekje2=content.lastIndexOf("##", hekje1+1);
-			System.out.println("H1: "+hekje1+ "H2:"+hekje2);
-			// alles tussen hekje1 en hekje2 is een language tag. controleer of die nog niet eerder gevonden is
-			String tag = content.substring(hekje1+1,hekje2);
-			if (!(languagereferences.contains(tag)))
+			System.out.println(arr[i]);
+			if (!(languagereferences.contains(arr[i])))
 			{
-				languagereferences.add(tag);
+				languagereferences.add(arr[i]);
 			}
 		}
 	}
@@ -75,15 +84,35 @@ public class CollectLanguageTags
 		pseudoservlets = new Vector<String>();
 		pseudoservlets.add("Search.java");
 		pseudoservlets.add("Schedule.java");
+		pseudoservlets.add("BuildingTable.java");
+		pseudoservlets.add("CourseTable.java");
+		pseudoservlets.add("EducatorTable.java");
+		pseudoservlets.add("PseudoServlet.java");
+		pseudoservlets.add("SingleTable.java");
+		pseudoservlets.add("StudentTable.java");
+		pseudoservlets.add("TabAndDrop.java");
+		pseudoservlets.add("TabAndList.java");
 		
 		templatefiles = new Vector<String>();
 		templatefiles.add("schedule.tpl");
 		templatefiles.add("search.tpl");
 		templatefiles.add("search2.tpl");
-		//templatefiles.add("simpletable.tpl");
-		//templatefiles.add("buildingTable.tpl");
-		//templatefiles.add("courseTable.tpl");
+		templatefiles.add("simpletable.tpl");
+		templatefiles.add("buildingTable.tpl");
+		templatefiles.add("courseTable.tpl");
 		
+		datastructure = new Vector<String>();
+		datastructure.add("Admin.java");
+		datastructure.add("Building.java");
+		datastructure.add("Course.java");
+		datastructure.add("Educator.java");
+		datastructure.add("Faculty.java");
+		datastructure.add("Hardware.java");
+		datastructure.add("Program.java");
+		datastructure.add("Room.java");
+		datastructure.add("Student.java");
+		datastructure.add("SubCourse.java");
+		datastructure.add("TypeOfCourse.java");
 	}
 	
 	
