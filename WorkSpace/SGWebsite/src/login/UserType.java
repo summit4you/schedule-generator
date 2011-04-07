4836b0c73e55459f27cdcb4d14e5220c8097cbb3
Lex.randea@gmail.com <Lex.randea@gmail.com@d2fbd850-ccee-fbfd-508f-682b2b5a58d6>
2011-04-07 16:38:03 +0000

    UserType aangepast

--- WorkSpace/SGWebsite/src/login/UserType.java
+++ WorkSpace/SGWebsite/src/login/UserType.java
@@ -2,20 +2,49 @@ package login;
 
 import java.util.Vector;
 
+import database.DatabasableAsString;
+
+import pseudoServlets.PseudoServlet;
+import sessionTracking.Session;
+import xml.ElementWithChildren;
+import xml.ElementWithValue;
+import xml.XMLDocument;
+import xml.XMLElement;
+
 import htmlBuilder.MainServlet;
 import htmlBuilder.Site;
 
-public abstract class UserType
+public class UserType implements DatabasableAsString
 {
+	
+	static protected XMLDocument typeDoc=loadTypeDoc();
+	static private final String defaultLocation="C:\\SVN\\WorkSpace\\SGWebsite\\src\\login\\UserTypes.xml";
 	protected Vector<String> pseudos;
+	protected String value;
 	
 	UserType()
 	{
 		pseudos = new Vector<String>();
+		value = new String();
 	}
 	
-	public abstract String buildMainSite(String baselink,String id);
+	public static XMLDocument loadTypeDoc()
+	{
+		XMLDocument doc=new XMLDocument(defaultLocation);
+		doc.load();
+		return doc;
+	}
+
+	public Vector<String> getPseudos()
+	{
+		return pseudos;
+	}
 	
+	public void setPseudos(Vector<String> pseudos)
+	{
+		this.pseudos = pseudos;
+	}
+		
 	public boolean isAuthorized(String ps)
 	{
 		for(String p:pseudos)
@@ -26,6 +55,47 @@ public abstract class UserType
 			}
 		}
 		return false;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public Site buildSite(Session ses)
+	{
+		Site site = new Site();
+		for(String p:pseudos)
+		{
+			site.addTabWithIFrame(PseudoServlet.getPseudoServlet(p).getTabName(), PseudoServlet.getPseudoServlet(p).createLink(ses));
+		}
+		return site;
+	}
+
+	@Override
+	public String toValue()
+	{
+		return value;
+	}
+
+	@Override
+	public void loadFromValue(String value)
+	{
+		pseudos = new Vector<String>();
+		XMLElement el = typeDoc.getElement(value);
+		if (el!=null)
+		{
+			try
+			{
+				for(XMLElement child:ElementWithChildren.class.cast(el).getElements())
+				{
+					pseudos.add(ElementWithValue.class.cast(child).getValue());
+				}
+				value=el.getName();
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+				
+		
 	}
 	
 
