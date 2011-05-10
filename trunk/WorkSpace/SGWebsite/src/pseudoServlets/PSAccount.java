7cd4969b0f16ecce41c960e243852e45c7a6cd89
adamcooman@gmail.com <adamcooman@gmail.com@d2fbd850-ccee-fbfd-508f-682b2b5a58d6>
2011-05-10 22:12:25 +0000

    account tab gemaakt, het ding moet wel nog is getest worden, maar het internet is hier te zwak om dat te doen

--- trunk/WorkSpace/SGWebsite/src/pseudoServlets/PSAccount.java
+++ trunk/WorkSpace/SGWebsite/src/pseudoServlets/PSAccount.java
@@ -2,6 +2,10 @@ package pseudoServlets;
 
 import javax.servlet.http.HttpServletRequest;
 
+import database.Database;
+
+import pseudoServlets.tools.PSTools;
+
 import sessionTracking.Session;
 
 import htmlBuilder.Site;
@@ -10,12 +14,17 @@ import htmlBuilder.Site;
  * <b>PseudoServlet that shows the user its account data</b> </br> </br> 
  * 
  * @version 0.1
- * @author Alexander
+ * @author Adam
  *
  */
 public class PSAccount extends PseudoServlet
 	{
 		
+		public PSAccount()
+		{
+			templateFile="account.tpl";
+		}
+	
 		@Override
 		protected void init()
 		{
@@ -25,21 +34,68 @@ public class PSAccount extends PseudoServlet
 		@Override
 		public String processRequest(RequestType type, HttpServletRequest request, Session session)
 		{
+			String response = replaceTags(template,"MASTERSERVLET", createLink(session));
+			response = replaceTags(response,"ACCOUNTNAME","##account_is##" + session.getAccount().getUserName());
+			response = replaceTags(response,"CURRENTLANGUAGE","##language_is##" + session.getAccount().getLanguage());
+			response = replaceTags(response,"LANGUAGEOPTIONS",PSTools.createLanguageOptions());
 			if (type==RequestType.POST)
 			{
-				//TODO receive data and process it
+				String lang = request.getParameter("lang");
+				if (lang==null)
+				{
+					// there has been a change password request
+					String pass1=request.getParameter("pass1");
+					String pass2=request.getParameter("pass2");
+					if ((pass1==null)||(pass2==null))
+					{
+						response = replaceTags(response,"PASSRESULT","##please_complete_form##");
+						response = replaceTags(response,"LANGRESULT"," ");
+					} 
+					else
+					{
+						if (pass1.equals(pass2))
+						{
+							response = replaceTags(response,"PASSRESULT","##password_change_success##");
+							response = replaceTags(response,"LANGRESULT"," ");
+							// TODO change password in database
+							Database db = getDB();
+							db.connect();
+							session.getAccount().setPassword(pass1);
+							db.write(session.getAccount());
+							db.disconnect();
+						}
+						else
+						{
+							response = replaceTags(response,"PASSRESULT","##passwords_dont_match##");
+							response = replaceTags(response,"LANGRESULT"," ");
+						}
+					}
+				}
+				else
+				{
+					// there has been a change language request
+					// TODO change the shit in the database
+					Database db = getDB();
+					db.connect();
+					session.getAccount().setLanguage(lang);
+					db.write(session.getAccount());
+					db.disconnect();
+					response = replaceTags(response,"PASSRESULT"," ");
+					response = replaceTags(response,"LANGRESULT","##language_changed##");
+				}
 			}
 			else
 			{
-				//TODO send data and form
+				response = replaceTags(response,"PASSRESULT"," ");
+				response = replaceTags(response,"LANGRESULT"," ");
 			}
-			return null;
+			return response;
 		}
 		
 		@Override
 		public String getTabName()
 		{
-			return Site.TabName.MyAccount.toLanguageTag();
+			return null; //Site.TabName.MyAccount.toLanguageTag();
 		}
 
 }
